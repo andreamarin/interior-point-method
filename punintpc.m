68552c4b4c7a077b29dc33f09976969cf65a0ff9
am <andrea.marin2411@gmail.com>
2020-10-03 11:16:19 -0500

    working version of interior point method

--- punintpc.m
+++ punintpc.m
@@ -29,7 +29,7 @@ n = length(c);     % dimension de la variable principal
 m = length(b);     % numero de restricciones de desigualdad
 %----------------------------------------------------------
 % variables iniciales
-x = ones(n,1);
+x = [1/2; -1]; %ones(n,1);
 mu = ones(m,1);
 y = ones(m,1);
 Y = diag(y);
@@ -40,67 +40,66 @@ sigma = 0.5;
 eta = sigma*(y'* mu)/m;
 %-----------------------------------------------
 % vectores para graficacion
-cnpo=[]; comp =[];
+%cnpo=[]; comp =[];
 
 % Condiciones necesarias de primer orden
-F =[Q*x - A'*mu +c; A*x - y - b; Y*U*e];
+F = vertcat(Q*x - A'*mu +c, A*x - y - b, Y*U*e);
 norma = norm(F);
-disp('Iter      CNPO             tau ')
-disp('-----------------------------------------')
 
-while(norma > tol & iter < maxiter)
+while(norma > tol && iter < maxiter)
   % Resolvemos sistema lineal de Newton para sigma = 0
-  r_x = Q*x - A'*mu + c
-  r_y = A*x - y - b
-  r_mu = Y*U*e - eta*e
+  r_x = Q*x - A'*mu + c;
+  r_y = A*x - y - b;
+  r_mu = Y*U*e - eta*e;
 
-  Y_inv = inv(Y)
+  Y_inv = inv(Y);
 
   % sistema lineal (5)
-  K = Q + A'*Y_inv*U*A
-  z = -(r_x + A'*Y_inv*U*r_y + Y_inv*r_mu)
-  Dx = K \ z
+  AT_Yinv = A'*Y_inv;
+  K = Q + AT_Yinv*U*A;
+  z = -(r_x + AT_Yinv*U*r_y + AT_Yinv*r_mu);
+  
+  Dx = K \ z;
 
   % resolvemos para dy y dmu
   Dy = A*Dx + r_y;
   Dmu = - Y_inv*( U*Dy + r_mu);
+  
   %-------------------------------------------------------
   % Recorte de paso
-  bt = []; 
-  gm = [];
+  bt = ones(m,1); 
+  gm = ones(m,1);
   for k=1:m
       if (Dmu(k) < 0)
-        bt = [bt; -(mu(k)/Dmu(k))];
+        bt(k) =  -mu(k)/Dmu(k);
       end
 
       if(Dy(k) < 0)
-          gm = [gm; -(y(k)/Dy(k))];
+          gm(k) = -y(k)/Dy(k);
       end
   end
   
-  alfa = min([bt ; gm]);
-  alfa =(0.9995)*min([1 alfa]);  
-  %-----------------------------------------------------------
+  alfa = min([bt gm]);
+  alfa =(0.995)*min([1 alfa]);
+  %------------------------------------------------
   % Actualizamos puntos
   x = x + alfa*Dx;
   mu = mu + alfa*Dmu;
   y = y + alfa*Dy;
+  
+  Y = diag(y);
+  U = diag(mu);
   %-------------------------------------------------------  
   % Nueva eta
   eta = sigma*(mu'*y)/m;
   %-------------------------------------------------------  
   % Calculamos otra vez las condiciones necesarias de primer orden
-  F =[Q*x - A'*mu +c; A*x - y - b; Y*U*e];
+  F = vertcat(Q*x - A'*mu +c, A*x - y - b, Y*U*e);
   norma = norm(F);
 
-  % actualizamos nÃºmero de iteraciones y arreglos
+  % actualizamos numero de iteraciones
   iter = iter + 1;
-  cnpo =[cnpo norma];
-  comp = [comp 2*eta];
-  disp(sprintf('%3.0f  %2.8f  %2.8f',iter,norma,2*eta)
 end
 
-semilogy([1:iter],cnpo,'r',[1:iter],comp,'b')
-title('Convergencia de puntos interiores')
-legend('CNPO', 'Complementaridad')
+disp(fprintf('finalizó en %3.0f iteraciones',iter))
     
