146a86060a67e271cd3f72109dc5c45b7c4c2806
am <andrea.marin2411@gmail.com>
2020-10-03 21:36:33 -0500

    ya funciona + script para lass ks

---
+++ descenso2pasos_qp.m
@@ -0,0 +1,65 @@
+function[W,H] = descenso2pasos(X,k)
+% Método de descenso en 2 pasos
+% Min ||X - WH||F^2 s.a W >=0, H>=0
+% In:
+% X: matriz rxp, Xij>=0
+% k<<r, k<<p
+% Out:
+% W: matriz 
+% H: matriz
+
+%------------------------------------
+% Optimización Numérica
+% 8 de octubre de 2020
+% ITAM
+%------------------------------------
+%Parámetros iniciales
+
+tol = 1e-04;
+[r, p] = size(X);
+W0 = ones(r,k);
+H0  = ones(k,p);
+kinit = k;
+k = 0;
+
+crit = 1;
+maxiter = 5;
+
+Wk = W0;
+Hk = H0;
+
+while crit >= tol && k < maxiter
+    Hk1 = ones(kinit,p);
+    A = eye(kinit);
+    b = zeros(kinit,1);
+    Q = Wk'*Wk; % matriz de kxk
+    for j=1:p
+        Xj = X(:,j); % vector de rx1
+        c = -(Xj'*Wk)'; % vector de 1xk
+        Hk1(:,j) = quadprog(Q, c, -A, b);
+    end
+    
+    Wk1 = ones(r,kinit);
+    Q = Hk1*Hk1';
+    for i=1:r
+        Xi = X(i,:);
+        c = -(Xi*Hk1')';
+        Wk1(i,:) = quadprog(Q, c, -A, b);
+    end
+    
+    crit = norm(Hk1 - Hk, 'fro')^2 + norm(Wk1 - Wk, 'fro')^2;
+    disp('criterio')
+    disp(crit)
+    
+    k = k+1;
+    
+    Wk = Wk1;
+    Hk = Hk1;
+end
+
+disp(sprintf('finalizó en %3.0f iteraciones',k))
+
+W = Wk1;
+H = Hk1;
+
+end
--- payaso.m
+++ payaso.m
@@ -1,11 +1,31 @@
 % cargar la imagen del payaso
 load clown
 colormap('gray')
-image(X)
-surf(X)
 % resolvemos el problema 
-k = 5;
-[W,H] = descenso2pasos(X,k);
-%[W,H] = nnmf(X,k);
-%surf(W*H)
-image(W*H)
+ks = [5, 20, 30, 60, 80];
+
+resultados = [];
+
+disp('k           tiempo punint       tiempo quadprog')
+for i=1:2
+    k = ks(i);
+    
+    % correr nuestro programa con puntos interiores
+    tic;
+    [W,H] = descenso2pasos(X,k); 
+    tEnd = toc;
+    
+    resultados = [resultados; [W,H]];
+    
+    % correr con quadprog
+    tic;
+    [Wqp,Hqp] = descenso2pasos_qp(X,k); 
+    tEnd_qp = toc;
+    
+    figure;
+    colormap('gray');
+    imshowpair(W*H, Wqp*Hqp, 'montage')
+    
+    disp(sprintf('%3.0f         %6.3f s         %6.3f s', k, tEnd, tEnd_qp))
+    pause(5)
+end
